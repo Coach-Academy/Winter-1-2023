7e3f07d9e80761ee72821a542271e08d81eb720c
Coach Academy <146649529+CoachAcademy@users.noreply.github.com>
2023-10-02 22:19:54 +0300

    Create Problem I.cpp

---
+++ Level 1 Class 1/Practice G1/Problem I.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //declaration
+    int n,m;
+    //input
+    cin>>m>>n;
+    //process and output
+    cout <<m*n/2<<endl;
+    return 0;
+}

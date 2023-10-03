dec7f1b642e7bfc87194cc042b0fa677316fa3a6
Coach Academy <146649529+CoachAcademy@users.noreply.github.com>
2023-10-03 19:16:23 +0300

    Create Problem M.cpp

---
+++ Level 1 Class 1/Practice G4/Week 2/Problem M.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+signed  main(){
+
+    int v,t,s,d;
+    cin>>v>>t>>s>>d;
+    int d1=v*t;
+    int d2=v*s;
+
+    if(d>=d1&&d<=d2)cout<<"No\n";
+    else cout<<"Yes\n";
+
+
+
+
+
+    return 0;
+}
+

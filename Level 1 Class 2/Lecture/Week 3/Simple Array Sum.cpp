d8ba0b721c1d57ba01dcc997b0a9cd69b9213dc8
Mohamed Amir <m.elamir027@gmail.com>
2023-10-04 00:05:10 +0300

    Create Simple Array Sum.cpp

---
+++ Level 1 Class 2/Lecture/Week 3/Simple Array Sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int sum = 0;
+
+    for (int i = 0; i < n; ++i) {
+        int s;
+        cin >> s;
+        sum += s;
+    }
+
+    cout << sum << endl;
+}

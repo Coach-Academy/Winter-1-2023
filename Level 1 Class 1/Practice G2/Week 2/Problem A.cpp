24f5d263328a23cf61c256ba321a83a1ba3f60d7
Coach Academy <146649529+CoachAcademy@users.noreply.github.com>
2023-10-03 18:18:39 +0300

    Create Problem A.cpp

---
+++ Level 1 Class 1/Practice G2/Week 2/Problem A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define TEKA ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main(){
+    TEKA
+    char c;
+    cin>>c;
+    if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')cout<<"vowel";
+    else cout<<"consonant";
+    return 0;
+}

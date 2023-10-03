8ff3ff32188287d2e3d46fc20722f20d32539ebd
Coach Academy <146649529+CoachAcademy@users.noreply.github.com>
2023-10-03 19:06:57 +0300

    Create Problem A.cpp

---
+++ Level 1 Class 1/Practice G4/Week 2/Problem A.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+signed  main(){
+
+    char c;
+    cin>>c;
+    switch (c){
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            cout<<"vowel";
+            break;
+        default:
+             cout<<"consonant";
+    }
+
+
+return 0;
+}
+
+
+
+
+
+
+

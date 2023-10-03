5bbb0ed85b695999e3e8c6a617194e598bb790b2
Abdelrahman Elzarka <108380629+Abdelrahmanelzarka@users.noreply.github.com>
2023-10-03 21:53:19 +0300

    Create code.cpp

---
+++ Level 2 Class 3/Lecture/Week 1/code.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+ 
+int main()
+{
+    int N,A;
+    cin>>N>>A;
+    int res1,res2;
+    res1=N*N;
+    res2=res1-A;
+    cout<<res2;
+   // cout<<N*N-A;
+ 
+ 
+    return 0;
+}
+ 
+/////////////////////////
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+ 
+int main()
+{
+  int A,B;
+  cin>>A>>B;
+ 
+  cout<<A*B;
+ 
+    return 0;
+}
+ 
+///////////////////
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+ 
+int main()
+{
+   int N,W;
+   cin>>N>>W;
+ 
+ 
+   //100 //4
+   int result;
+ 
+    result=N/W;
+ 
+    cout<<result;
+    /////////
+  //  int counter=0;
+ 
+   // while(N>=W)
+    //{
+      //  N-=W;
+       // counter++;
+    //}
+   // cout<<counter;
+ 
+    return 0;
+}
+ 
+ 
+/////////

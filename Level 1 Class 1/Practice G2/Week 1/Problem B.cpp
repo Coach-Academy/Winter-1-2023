ca21a04590d141d1d4f44953eb11a3e89cf037d1
7oSkaaa <ahmed.7oskaa@gmail.com>
2023-10-03 15:17:21 +0300

    refactor: re structure the folders

---
+++ Level 1 Class 1/Practice G2/Week 1/Problem B.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+signed  main(){
+
+    int a,b;
+    cin>>a>>b;
+    cout<<a*b<<'\n';
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
---
+++ Level 1 Class 1/Practice G2/Week 1/Problem L.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+signed main(){
+      int n;
+      cin>>n;
+      cout<<n/100+(n/20)%5+(n/10)%2+(n/5)%2+(n)%5<<'\n';
+
+    return 0;
+}
+

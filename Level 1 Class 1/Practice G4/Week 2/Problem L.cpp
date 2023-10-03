9181c745473a8e4a7873c3548352db81e0043cc2
Coach Academy <146649529+CoachAcademy@users.noreply.github.com>
2023-10-03 19:12:30 +0300

    Create Problem L.cpp

---
+++ Level 1 Class 1/Practice G4/Week 2/Problem L.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+signed  main(){
+
+
+    int n;
+    cin>>n;
+    if(n<40)cout<<40-n<<'\n';
+    else if (n<70)cout<<70-n<<'\n';
+    else if(n<90)cout<<90-n<<'\n';
+    else cout<<"expert\n";
+
+
+
+
+    return 0;
+}
+

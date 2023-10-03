f186c30d07f4cdeb54b73de387a8f49d42c03d81
Coach Academy <146649529+CoachAcademy@users.noreply.github.com>
2023-10-03 18:58:05 +0300

    Create Problem D.cpp

---
+++ Level 1 Class 1/Practice G2/Week 2/Problem D.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define TEKA ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main(){
+    TEKA
+    int n;
+    cin>>n;
+    if(n%2==0 && n!=2)cout<<"YES";
+    else cout<<"NO";
+    return 0;
+}

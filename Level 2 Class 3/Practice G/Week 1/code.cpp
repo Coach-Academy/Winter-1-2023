ff02d6b039e9672f0bfc1d80a5d09b218baece77
Abdelrahman Elzarka <108380629+Abdelrahmanelzarka@users.noreply.github.com>
2023-10-03 21:54:19 +0300

    Create code.cpp

---
+++ Level 2 Class 3/Practice G/Week 1/code.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    /// problem D
+    /*int a,b,c;
+    cin>>a>>b>>c;
+    int e,t,g;
+    e=7-a;
+    t=7-b;
+    g=7-c;
+    int res=g+t+e;
+    cout<<res;*/
+
+
+    ///problem E
+/*
+    int M;
+    cin>>M;
+    cout<<48-M;*/
+
+    ///problem g
+    /*int A,B;
+    double C;
+    cin>>A>>B;
+
+    C=((A-B)*1.0)/3.0+B;
+    cout<<C;*/
+
+    ///problem H
+
+   /* float A,B;
+    cin>>A>>B;
+    float res;
+
+    res=A/100*B;
+
+
+    cout<<res;*/
+
+    /// problem I
+
+    /*int N,M;
+    cin>>N>>M;
+    cout<<N*M/2;*/
+
+    ///problem J
+   /* int x;
+    cin>>x;
+    cout<<(x+4)/5;*/
+    ///problem k
+    /*
+    int l,p,q;
+    double time,distance;
+    cin>>l>>p>>q;
+    time=l*1.0/(p+q);
+    distance=time*p;
+
+    cout<<distance;*/
+
+    ///problem l
+    /*int n;
+    cin>>n;
+    int hundred=n/100;
+    n%=100;
+    int twenty=n/20;
+    n%=20;
+    int ten=n/10;
+    n%=10;
+    int five=n/5;
+    n%=5;
+    int one=n;
+    cout<<hundred+twenty+ten+five+one;*/
+
+
+
+    return 0;
+}
+
+
+
+
+

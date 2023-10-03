0d41ed32f588120a2df6fa7145308e931deffd38
Coach Academy <146649529+CoachAcademy@users.noreply.github.com>
2023-10-03 19:11:05 +0300

    Create Problem J.cpp

---
+++ Level 1 Class 1/Practice G4/Week 2/Problem J.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+signed  main(){
+
+  int a,b,c;
+  cin>>a>>b>>c;
+
+  if(a>b||(a==b&&c==1))cout<<"Takahashi\n";
+  else cout<<"Aoki\n";
+
+
+
+
+
+    return 0;
+}
+

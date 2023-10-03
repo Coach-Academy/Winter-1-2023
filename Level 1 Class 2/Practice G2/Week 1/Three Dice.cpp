9cf8e3a4d919cef7caa594a1a0bedb465b3e8812
Mohamed Amir <m.elamir027@gmail.com>
2023-10-03 19:54:05 +0300

    Create Three Dice.cpp

---
+++ Level 1 Class 2/Practice G2/Week 1/Three Dice.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    int l = 7 - a, z = 7 - b, w = 7 - c;
+    int sum = l + z + w;
+    cout << sum << endl;
+}

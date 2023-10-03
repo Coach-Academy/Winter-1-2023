6dc1351ca4b24ce65dfcea6513bfbe4a1ae56275
Coach Academy <146649529+CoachAcademy@users.noreply.github.com>
2023-10-03 19:01:06 +0300

    Create Problem H.cpp

---
+++ Level 1 Class 1/Practice G2/Week 2/Problem H.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define TEKA ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main(){
+    TEKA
+    int n,k;
+    cin>>n>>k;
+    if(n*500>=k) cout<<"Yes";
+    else cout<<"No";
+
+    return 0;
+}

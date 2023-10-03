0a899e78de70e94690b523c9795fc625abc1408c
Coach Academy <146649529+CoachAcademy@users.noreply.github.com>
2023-10-03 19:07:51 +0300

    Create Problem D.cpp

---
+++ Level 1 Class 1/Practice G4/Week 2/Problem D.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+signed  main(){
+
+
+    int w;
+    cin>>w;
+    if(w%2==0&&w!=2)cout<<"YES\n";
+    else cout<<"NO\n";
+
+
+
+    return 0;
+}
+

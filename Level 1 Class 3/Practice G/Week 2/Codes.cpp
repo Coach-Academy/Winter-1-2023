c4b2dba024ee48890465056bd29890b3a356e4af
Coach Academy <146649529+CoachAcademy@users.noreply.github.com>
2023-10-03 19:35:47 +0300

    Create Codes.cpp

---
+++ Level 1 Class 3/Practice G/Week 2/Codes.cpp
@@ -0,0 +1,186 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+
+
+int main()
+{
+    ///problem A
+   /*char c;
+   cin>>c;
+
+   if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u')
+   {
+       cout<<"vowel";
+   }
+   else
+   {
+       cout<<"consonant";
+   }*/
+   ///problem B
+
+  /* int w;
+   cin>>w;
+
+   if(w%2==0 && w>2)
+   {
+    cout<<"YES";
+   }
+   else
+    cout<<"NO";*/
+
+   /* /// problem c
+
+    int a,b,sum,multi,mins;
+    cin>>a>>b;
+    sum=a+b;
+    multi=a*b;
+    mins=a-b;
+    if(sum>=multi && sum>=mins)
+        cout<<sum;
+    else if(mins>=multi && mins>=sum)
+        cout<<mins;
+    else
+        cout<<multi;*/
+
+
+        /// problem theater square
+        /// n & m dimensions for pivot
+        /// a dimension of brick
+
+
+       /* int n,m,a;
+        cin>>n>>m>>a;
+
+        int sum1=n*m,sum2=a*a;
+
+        int total=(sum1+sum2-1)/sum2;
+        cout<<total;
+
+        int width=(n+a-1)/a;
+        int height= (m+a-1)/a;
+
+
+
+        int total=width*height;
+        cout<<total;
+
+
+        cout<<"kam toba";*/
+
+
+
+
+
+
+
+/// just explanation for the lecture
+    /*cout<<x<<"\n";
+
+    if(true)
+    {
+        x=7;
+        cout<<x<<"\n";
+        x++;
+
+    }
+
+
+
+    cout<<x<<"\n";*/
+
+
+
+  /*
+  int a=100;
+
+  if(a>60)
+  {
+      cout<<"aaa";
+  }
+
+  if(a>40)
+  {
+
+      cout<<"bbb";
+  }
+  else
+  {
+      cout<<"ccc";
+  }*/
+
+  /*int a,b,c;
+  cin>>a>>b>>c;
+
+  if(b>a)
+  {
+     if(b>c)
+        cout<<b;
+     else {
+        cout<<c;
+     }
+  }
+  else{
+        if(a>c)
+        cout<<a;
+       else {
+        cout<<c;
+     }
+  }*/
+
+ /* string a="red";
+  switch(a)
+      {
+
+
+  case "red":
+      cout<"3";
+    break;
+  case 6:
+    cout<<"6";
+    break;
+  case 7:
+      cout<<"7";
+    break;
+  default:
+      cout<<"not found";
+      }*/
+   /*   int a,b;
+      char c;
+      cin>>a>>b>>c;
+
+      switch(c){
+  case '+':
+    cout<<a+b;
+    break;
+    case '-':
+    cout<<a-b;
+    break;
+    case '*':
+    cout<<a*b;
+    break;
+
+    default:
+        cout<<a/b;
+
+
+
+
+      }*/
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}

be045f567b5881e0a69b3d9e15159e02f8ae5f5c
Abdelrahman Elzarka <108380629+Abdelrahmanelzarka@users.noreply.github.com>
2023-10-04 23:32:34 +0300

    Create Code.cpp

---
+++ Level 1 Class 3/Lecture/Week 3/Code.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{/*
+    int n=10;
+
+
+    for(int i=0 ; i<n ; i++)
+    {
+        cout<<i<<"\n";
+
+    }
+
+
+    cout<<n<<"\n";
+    */
+   /* int t;
+    cin>>t;
+    while(t--)
+    {
+
+    }*/
+
+
+   /* int res=1;
+    int n;
+    cin>>n;
+    while(n)
+    {
+        res*=n;
+        n--;
+
+    }
+    cout<<res<<"\n";*/
+
+   /* int y=0;
+
+    for(int ppl=9870 ; ppl<=30000 ; ppl+=ppl*0.1)
+    {
+       y++;
+    }
+    cout<<y;*/
+
+  /* int i=0;
+   int x=2;
+
+   while(i<100)
+   {
+       for(int j=0 ; j<10 ; j++)
+          cout<<j;
+
+        i++;
+
+
+
+
+   }*/
+   /*
+char out, in;
+for (out = 'N' ; out >= 'A' ; out--) {
+for (in = 'A' ; in <= out; in++)
+    cout <<in;
+cout<< "\n";
+}*/
+
+/*for(int i=5 ; i>0 ; i--)
+{
+    for(int j=1 ; j<=i ; j++)
+        cout<<"*";
+    cout<<"\n";
+}*/
+
+
+///problem A
+/*
+int n,res=0;
+cin>>n;
+res=(n*(n+1))/2;
+
+/*for(int i=1; i<=n ; i++)
+{
+    res+=i;
+}*/
+//cout<<res;
+
+///problem b
+/*
+int p;
+for(int i=1 ; i<=26; i++)
+{
+   cin>>p;
+   cout<<char(96+p);
+}*/
+
+///problem c
+/*
+
+int n,p,q,rooms=0;
+cin>>n;
+
+for(int i=1 ; i<=n ; i++ )
+{
+    cin>>p>>q;
+    if(q>=2+p)
+    {
+        rooms++;
+    }
+
+
+}
+cout<<rooms;*/
+
+
+
+
+
+    return 0;
+}

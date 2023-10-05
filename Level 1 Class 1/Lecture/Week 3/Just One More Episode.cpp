a8493c9f19da657f3f8b62c3a21f96087fc3375f
Mohamed Amir <m.elamir027@gmail.com>
2023-10-05 18:39:09 +0300

    Create Just One More Episode.cpp

---
+++ Level 1 Class 1/Lecture/Week 3/Just One More Episode.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int x;
+        cin>>x;
+        if(x<=24)cout<<"No\n";
+        else cout<<"Yes\n";
+    }
+}

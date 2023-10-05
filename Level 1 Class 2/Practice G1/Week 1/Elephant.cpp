dd6d28ff1be809a2acce59dd4b3cb9e2511bbdfc
kurabika44 <46005424+MahmoudMkhimer@users.noreply.github.com>
2023-10-06 01:45:05 +0300

    Update Elephant.cpp

--- Level 1 Class 2/Practice G1/Week 1/Elephant.cpp
+++ Level 1 Class 2/Practice G1/Week 1/Elephant.cpp
@@ -3,7 +3,18 @@
 #define endl "\n"
 using namespace std;
 int main() {
-    int a,b;
-    cin>>a>>b;
-    cout<<a*b/2;
+    int n;cin>>n;
+    int steps = n/5;
+    n%=5;
+    steps =steps + n/4;
+    n= n%4;
+    steps =steps + n/3;
+    n= n%3;
+    steps =steps + n/2;
+    n= n%2;
+    steps =steps + n/1;
+    n= n%1;
+    cout<<steps;
+
+
 }

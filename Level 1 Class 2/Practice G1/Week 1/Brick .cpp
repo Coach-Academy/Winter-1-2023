a3ce1293e4f20f59db4a4b790b7d0fa76e247beb
kurabika44 <46005424+MahmoudMkhimer@users.noreply.github.com>
2023-10-06 00:58:30 +0300

    Create Brick .cpp

---
+++ Level 1 Class 2/Practice G1/Week 1/Brick .cpp
@@ -0,0 +1,10 @@
+//https://vjudge.net/contest/582731#problem/A
+#include <iostream>
+#define endl "\n"
+using namespace std;
+int main() {
+    int n,k;
+    cin>>n>>k;
+    cout<<n/k;
+    return 0;
+}

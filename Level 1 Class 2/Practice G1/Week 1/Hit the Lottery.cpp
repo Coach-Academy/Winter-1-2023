f4335f3d6f2c4ef9d99026550eab90d649205b84
kurabika44 <46005424+MahmoudMkhimer@users.noreply.github.com>
2023-10-06 01:46:31 +0300

    Create Hit the Lottery.cpp

---
+++ Level 1 Class 2/Practice G1/Week 1/Hit the Lottery.cpp
@@ -0,0 +1,20 @@
+//https://vjudge.net/contest/582731#problem/L
+#include <iostream>
+#define endl "\n"
+using namespace std;
+int main() {
+    int n;cin>>n;
+    int steps = n/5;
+    n%=5;
+    steps =steps + n/4;
+    n= n%4;
+    steps =steps + n/3;
+    n= n%3;
+    steps =steps + n/2;
+    n= n%2;
+    steps =steps + n/1;
+    n= n%1;
+    cout<<steps;
+
+
+}

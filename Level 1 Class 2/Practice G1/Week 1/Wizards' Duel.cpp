7db6218825fdd913c7ea62a0a836f4017da37234
kurabika44 <46005424+MahmoudMkhimer@users.noreply.github.com>
2023-10-06 01:44:33 +0300

    Create Wizards' Duel.cpp

---
+++ Level 1 Class 2/Practice G1/Week 1/Wizards' Duel.cpp
@@ -0,0 +1,11 @@
+//https://vjudge.net/contest/582731#problem/K
+#include <iostream>
+#define endl "\n"
+using namespace std;
+int main() {
+    double d,a,b;cin>>d>>a>>b;
+    double time = d/(a+b);
+    cout<< time*a;
+
+
+}

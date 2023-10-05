3bf7240e627c196a2b2c6e5bb5c9c45f7c6dbb22
Mohamed Amir <m.elamir027@gmail.com>
2023-10-05 22:55:13 +0300

    Create Palindromic Number (1).cpp

---
+++ Level 1 Class 2/Practice G2/Week 2/Palindromic Number (1).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char a, b, c;
+    cin >> a >> b >> c;
+    if (a == c)
+        cout << "Yes\n";
+    else
+        cout << "No\n";
+}

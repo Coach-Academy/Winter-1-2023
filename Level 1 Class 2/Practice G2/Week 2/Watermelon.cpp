6d6a47997b54a8051ae0a63730df6c83b9223cf6
Mohamed Amir <m.elamir027@gmail.com>
2023-10-05 22:53:51 +0300

    Create Watermelon.cpp

---
+++ Level 1 Class 2/Practice G2/Week 2/Watermelon.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int w;
+    cin >> w;
+    if (w == 2 or w % 2 == 1)
+        cout << "NO\n";
+    else
+        cout << "YES\n";
+}

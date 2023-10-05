5a0bd2e07d2bd97bb2c0c0ec8f46a9386d0197b4
Mohamed Amir <m.elamir027@gmail.com>
2023-10-05 22:55:49 +0300

    Create Day of Takahashi.cpp

---
+++ Level 1 Class 2/Practice G2/Week 2/Day of Takahashi.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if (b >= a)
+        cout << a << endl;
+    else
+        cout << a - 1 << endl;
+}

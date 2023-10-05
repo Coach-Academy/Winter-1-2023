cecbe0df21a1328b979c0a7e8379d67974c4181c
Mohamed Amir <m.elamir027@gmail.com>
2023-10-05 22:53:08 +0300

    Create +-x.cpp

---
+++ Level 1 Class 2/Practice G2/Week 2/+-x.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if (a + b >= a - b and a + b >= a * b)
+        cout << a + b << endl;
+    else if (a - b >= a + b and a - b >= a * b)
+        cout << a - b << endl;
+    else
+        cout << a * b << endl;
+}

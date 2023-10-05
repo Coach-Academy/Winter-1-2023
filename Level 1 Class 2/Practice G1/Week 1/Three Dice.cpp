6a6f2c2a344cff5bcc5e0b171d9ed231038f41b1
kurabika44 <46005424+MahmoudMkhimer@users.noreply.github.com>
2023-10-06 01:02:25 +0300

    Create Three Dice.cpp

---
+++ Level 1 Class 2/Practice G1/Week 1/Three Dice.cpp
@@ -0,0 +1,10 @@
+//https://vjudge.net/contest/582731#problem/D
+#include <iostream>
+#define endl "\n"
+using namespace std;
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<21-a-b-c;
+
+}

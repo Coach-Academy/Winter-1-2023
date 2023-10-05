91bee531b4a1976a462b8f5ee1774b982f531ec7
kurabika44 <46005424+MahmoudMkhimer@users.noreply.github.com>
2023-10-05 18:42:42 +0300

    Create Palindromic Number .cpp

---
+++ Level 1 Class 2/Practice G1/Week 2/Palindromic Number .cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "vector"
+#include "queue"
+#define endl "\n"
+#include "algorithm"
+using namespace std;
+// https://vjudge.net/contest/583996#problem/F
+int main() {
+    cin.tie(0), cout.tie(0), cin.sync_with_stdio(0), cout.sync_with_stdio(0);
+    int n;cin>>n;
+    cout<<(((n/100)==n%10)?"Yes":"No");
+
+}
+

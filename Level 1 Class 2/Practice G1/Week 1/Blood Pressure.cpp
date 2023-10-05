8ffeec3b2bd19d32a2569ecf7d2b491b5dd3bad7
kurabika44 <46005424+MahmoudMkhimer@users.noreply.github.com>
2023-10-06 01:06:24 +0300

    Create Blood Pressure.cpp

---
+++ Level 1 Class 2/Practice G1/Week 1/Blood Pressure.cpp
@@ -0,0 +1,9 @@
+//https://vjudge.net/contest/582731#problem/G
+#include <iostream>
+#define endl "\n"
+using namespace std;
+int main() {
+    int a,b;cin>>a>>b;
+    double c = (a-b)/3.0 + b;
+    cout<<c;
+}

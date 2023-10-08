45fafd7519bcb12e08c02eddf5f4004811761332
kurabika44 <46005424+MahmoudMkhimer@users.noreply.github.com>
2023-10-08 20:07:15 +0300

    Create qwerty.cpp

---
+++ Level 1 Class 2/Practice G1/Week 03/qwerty.cpp
@@ -0,0 +1,22 @@
+//https://vjudge.net/contest/585426#problem/B
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+int main(){
+    for(int i= 0;i<26;i++){
+        int val;cin>>val;
+        cout<<char(96+val);
+    }
+    return 0;
+}
+
+
+
+
+
+
+

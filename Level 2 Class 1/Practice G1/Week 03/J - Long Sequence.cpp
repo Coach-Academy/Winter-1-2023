57569e7406354017481bf7ef10b28c31a9bfacf9
7oSkaaa <ahmed.7oskaa@gmail.com>
2023-10-10 21:23:44 +0300

    refactor: add solutions for week 3

---
+++ Level 2 Class 1/Practice G1/Week 03/J - Long Sequence.cpp
@@ -0,0 +1,68 @@
+// Problem url: https://vjudge.net/contest/585428#problem/J
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fixed(n) fixed << setprecision(n)
+#define ceil(n, m) (((n) + (m) - 1) / (m))
+#define add_mod(a, b, m) (((a % m) + (b % m)) % m)
+#define sub_mod(a, b, m) (((a % m) - (b % m) + m) % m)
+#define mul_mod(a, b, m) (((a % m) * (b % m)) % m)
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define sz(x) int(x.size())
+#define debug(x) cout << #x << ": " << (x) << "\n";
+#define fi first
+#define se second
+#define ll long long
+#define ull unsigned long long
+#define EPS 1e-9
+constexpr int INF = 1 << 30, Mod = 1e9 + 7;
+constexpr ll LINF = 1LL << 62;
+#define PI acos(-1)
+template < typename T = int > using Pair = pair < T, T >;
+vector < string > RET = {"NO", "YES"};
+
+template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
+    for (auto &x : v) in >> x;
+    return in;
+}
+
+template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) { 
+    for (const T &x : v) out << x << ' '; 
+    return out;
+}
+
+void Solve(){
+    int n;
+    cin >> n;
+    vector < ll > a(n);
+    ll sum = 0;
+    for(auto& x : a)
+        cin >> x, sum += x;
+    ll x;
+    cin >> x;
+    ll cycles = x / sum;
+    x %= sum;
+    for(int i = 0; i < n; i++){
+        if(x >= a[i])
+            x -= a[i];
+        else {
+            cout << cycles * n + (i + 1) << '\n';
+            return;
+        }
+    }
+    cout << (cycles + 1) * n << '\n';
+}
+
+int main(){
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int test_cases = 1;
+    // cin >> test_cases;
+    for(int tc = 1; tc <= test_cases; tc++){
+        // cout << "Case #" << tc << ": ";
+        Solve();
+    }
+    return 0;
+}

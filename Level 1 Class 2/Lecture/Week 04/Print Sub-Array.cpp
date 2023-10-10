c42cb546813db3a483a96df76c0316f7ff6a053a
Mohamed Amir <m.elamir027@gmail.com>
2023-10-11 01:17:48 +0300

    Create Print Sub-Array.cpp

---
+++ Level 1 Class 2/Lecture/Week 04/Print Sub-Array.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    int l, r;
+    cin >> l >> r;
+    l--, r--;
+    for (int i = l; i <= r; ++i) {
+        cout << arr[i] << ' ';
+    }
+
+}

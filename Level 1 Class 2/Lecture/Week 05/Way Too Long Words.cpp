734db27099a5a61089f4658b8dc803ee312deffa
Mohamed Amir <m.elamir027@gmail.com>
2023-10-18 00:33:52 +0300

    Create Way Too Long Words.cpp

---
+++ Level 1 Class 2/Lecture/Week 05/Way Too Long Words.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    while (n--) {
+        string s;
+        cin >> s;
+        if (s.size() > 10) {
+            cout << s.front() << s.size() - 2 << s.back() << endl;
+        } else {
+            cout << s << endl;
+        }
+    }
+}

86174c976bc77de93bee68e467afe49f832766e7
Mohamed Amir <m.elamir027@gmail.com>
2023-10-22 00:12:07 +0300

    Create K - Pangram.cpp

---
+++ Level 1 Class 2/Practice G2/Week 05/K - Pangram.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n < 26)
+        cout << "NO\n";
+    else {
+        string s;
+        cin >> s;
+        for (int i = 0; i < s.size(); ++i)
+            s[i] = tolower(s[i]);
+
+        bool ans = 1;
+        for (char i = 'a'; i <= 'z'; ++i) {
+            if (s.find(i) == -1)
+                ans = 0;
+        }
+        if (ans)
+            cout << "YES\n";
+        else cout << "NO\n";
+    }
+}

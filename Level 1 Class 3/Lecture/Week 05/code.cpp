d3f1bd0267a320d875fb4e0dfefce7a5ecf353a1
Abdelrahman Elzarka <108380629+Abdelrahmanelzarka@users.noreply.github.com>
2023-10-23 00:18:58 +0300

    Create code.cpp

---
+++ Level 1 Class 3/Lecture/Week 05/code.cpp
@@ -0,0 +1,127 @@
+#include <bits/stdc++.h>
+#include<string>
+
+
+using namespace std;
+
+int main()
+{
+    /// Array of char
+   /* char a[100]="dog dog dog";
+
+
+
+
+    char b[10][10]={"hello","world"};
+    //char d[20]="cat";
+
+    //strcat(d,a);
+   // a->>2003 2004 2005 2006
+
+    char* p=strchr(a,'g');
+   // p=nullptr
+  // int b[100]={};
+
+   // cout<<p-a ;
+
+   /* strchr()
+    strcpy(a,d)
+
+
+
+     cout<<d;*/
+
+
+     /// Strings
+
+ /*    string s1,s2="cat dog cow";
+   /*  if(s1==s2)
+        if(s1>s2)*/
+        /*s2.erase(4,3);
+        s2.insert(4,"bird");*/
+        //s2.replace(4,3,"bird");
+     /*   int index=s2.find("t4 d");
+        cout<<index<<"\n";
+
+
+     cout<<s2<<"\n";
+     reverse(s2.begin(),s2.end());
+     //sort(s2.begin(),s2.end());
+     cout<<s2;
+     s2[1]='o';
+    /* cin>>s1;
+
+     for(int i=0 ; i<s1.size() ; i++)
+     {
+         cout<<s1[i];
+     }
+     cout<<"\n"<<s1.empty()<<"\n";
+     s1.clear();
+     cout<<s1.empty();*/
+   //  s2.push_back('W');
+     //cout<<s2;
+
+   ///problem B
+  /* int n;
+
+   cin>>n;
+   while(n--)
+   {
+       string word;
+       cin>>word;
+       if(word.size()<=10)
+       {
+           cout<<word<<"\n";
+       }
+       else
+       {
+           cout<<word[0];
+           cout<<word.size()-2;
+           cout<<word[word.size()-1];
+
+           cout<<"\n";
+
+       }
+       }*/
+
+
+
+
+  /* ///problem F
+
+
+
+     string n1,n2;
+
+     cin>>n1;
+
+
+     for(int i=n1.size()-1; i>=0 ; i--)
+     {
+         if(n1[i]=='0')
+         {
+            n1.erase(i,1);
+         }
+         else
+         {
+             break;
+         }
+     }
+    //cout<<n1;
+     n2=n1;
+     reverse(n2.begin(),n2.end());
+     if(n1==n2)
+     {
+         cout<<"Yes";
+     }
+     else
+     {
+        cout<<"No";
+     }*/
+
+     int x=(6==5? 5: 0);
+     cout<<x;
+
+
+    return 0;
+}

238dde44013f61c1cc922e5948dd7444f193ab26
Abdelrahman Elzarka <108380629+Abdelrahmanelzarka@users.noreply.github.com>
2023-10-25 23:42:27 +0300

    Create code.cpp

---
+++ Level 1 Class 3/Lecture/Week 06/code.cpp
@@ -0,0 +1,161 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+/*
+int hello1(string p)
+{
+    cout<<"hello "<<p;
+
+    return 0;
+}
+
+void hello2(string p)
+{
+
+cout<<"hello "<<p;
+
+}*/
+
+/*
+int f=10;
+
+int doubble(int c)
+{
+    return 2*c;
+}
+
+int summtion(int a,int b)
+{
+
+     cout<<b<<"\n";
+    return a+b;
+}*/
+
+/*
+double average(double math,double science , double english)
+{
+    return (math+science+english)/3.0;
+}
+
+char grade (double avg)
+{
+    if(avg>90)
+        return 'A';
+    else if (avg>70)
+      return 'B';
+    else
+      return 'F';
+}*/
+
+
+
+int main() {
+
+
+  /* int a=10,b=5;
+   int c=summtion(a,b);
+   cout<<c<<"\n";
+   cout<<a<<" "<<b;*/
+
+  /* for(int i=0 ; i<5 ; i++)
+   {
+       string name;
+       cin>>name;
+       double math,science,english;
+       cin>>math>>science>>english;
+       double avg=average(math,science,english);
+       char g=grade(avg);
+
+       cout<<"the name: "<<name<<"\n"<<"the average "<<avg<<"\nthe grade "<<g<<"\n";
+
+
+
+
+
+
+
+
+   }*/
+
+  /* double p=3.1234867;
+
+   cout<<setprecision(5)<<p;*/
+
+/*
+   int a=3,b=6;
+   cout<<min(a,min(3,min(8,6)));*/
+
+  /* int a[4]={5, 4 ,8 ,2};
+
+   sort(a,a+4);
+   for(int i=0 ; i<4 ; i++)
+    cout<<a[i]<<" ";
+
+    string s="zxy"
+    sort(s.begin(),s.end())
+
+   sort(a,a+4,greater<int>());
+
+   cout<<"\n";
+   for(int i=0 ; i<4 ; i++)
+    cout<<a[i]<<" ";*/
+
+   // int arr[4]={1,3,4,4};
+/*
+    do {
+
+        for (int i = 0; i < 4; i++)
+
+            cout << arr[i] << ' ';
+
+        cout << '\n';
+
+    } while(next_permutation(arr, arr + 4));*/
+  //  cout<<count(arr,arr+4,4);
+
+
+  /// problem A
+ /* int n,a[101];
+  cin>>n;
+  for(int i=0 ; i<n ; i++)
+    cin>>a[i];
+
+  sort(a,a+n);
+
+   for(int i=0 ; i<n ; i++)
+    cout<<a[i]<<" ";*/
+
+///problem B
+   /* int n;
+    cin>>n;
+    int a[1001];
+
+    for(int i=0 ; i<n ; i++)
+        cin>>a[i];
+
+
+ if(count(a,a+n,1)==0)
+    cout<<1;
+ else
+    cout<<-1;*/
+
+    /// problem C
+
+   /* string s,t;
+   cin>>s>>t;
+   code
+   edoc
+
+   reverse(s.begin(),s.end());
+
+   if(t==s)
+    cout<<"YES";
+   else
+    cout<<"NO";
+*/
+
+
+
+   return 0;
+}

7a37e6f52c47b8e8ade909fe6cfd8a27a6105caa
Mohamed Amir <m.elamir027@gmail.com>
2023-10-28 23:38:10 +0200

    Create C - Translation.cpp

---
+++ Level 1 Class 2/Practice G2/Week 06/C - Translation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    string s, t;
+    cin >> s >> t;
+    reverse(s.begin(), s.end());
+    if (s == t)
+        cout << "YES\n";
+    else cout << "NO\n";
+}

297b2d3945d73083e264158b5d9aaa29538c1dfd
Mohamed Amir <m.elamir027@gmail.com>
2023-10-28 23:40:21 +0200

    Create Is your horseshoe on the other hoof?.cpp

---
+++ Level 1 Class 2/Practice G2/Week 06/Is your horseshoe on the other hoof?.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int arr[4];
+    for (int i = 0; i < 4; ++i) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + 4);
+    int ans = 0;
+    for (int i = 0; i < 3; ++i) {
+        if (arr[i] == arr[i + 1])
+            ans++;
+    }
+    cout << ans << endl;
+}

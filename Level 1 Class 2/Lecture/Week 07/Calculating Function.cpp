8a5300dc0419fa5dbae423bd588e8ee206fde12e
Mohamed Amir <m.elamir027@gmail.com>
2023-10-31 23:57:58 +0200

    Create Calculating Function.cpp

---
+++ Level 1 Class 2/Lecture/Week 07/Calculating Function.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() { // o(1)
+    long long n;
+    cin >> n;
+    if (n % 2 == 0) {
+        cout << n / 2 << endl;
+    } else {
+        cout << -((n + 1) / 2) << endl;
+    }
+}

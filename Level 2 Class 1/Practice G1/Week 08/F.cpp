d64ed31540c5c7f00aaa2ee9aa80b5a448fbfc03
7oSkaaa <ahmed.7oskaa@gmail.com>
2023-11-29 01:05:58 +0200

    add codes for level 2 class 1 G1

---
+++ Level 2 Class 1/Practice G1/Week 08/F.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fixed(n) fixed << setprecision(n)
+#define ceil(n, m) (((n) + (m) - 1) / (m))
+#define add_mod(a, b, m) (((a % m) + (b % m)) % m)
+#define sub_mod(a, b, m) (((a % m) - (b % m) + m) % m)
+#define mul_mod(a, b, m) (((a % m) * (b % m)) % m)
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define sz(x) int(x.size())
+#define debug(x) cout << #x << ": " << (x) << "\n";
+#define fi first
+#define se second
+#define ll long long
+#define ull unsigned long long
+#define EPS 1e-9
+constexpr int INF = 1 << 30, Mod = 1e9 + 7;
+constexpr ll LINF = 1LL << 62;
+#define PI acos(-1)
+template < typename T = int > using Pair = pair < T, T >;
+vector < string > RET = {"NO", "YES"};
+
+template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
+    for (auto &x : v) in >> x;
+    return in;
+}
+
+template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) { 
+    for (const T &x : v) out << x << ' '; 
+    return out;
+}
+
+void Solve(){
+    string a, b;
+    cin >> a >> b;
+    int n = sz(a);
+    int res_a = 0, res_b = 0, question_marks = 0;
+    for(int i = 0; i < n; i++){
+        res_a += (a[i] == '+' ? 1 : -1);
+        if(b[i] == '?') question_marks++;
+        else res_b += (b[i] == '+' ? 1 : -1);
+    }
+    int success = 0, total = 1 << question_marks;
+    for(int mask = 0; mask < total; mask++){
+        int curr_sum = 0;
+        for(int i = 0; i < question_marks; i++)
+            curr_sum += ((mask >> i) & 1 ? 1 : -1);
+        success += (res_a == res_b + curr_sum);
+    }
+    cout << fixed(12) << 1.0 * success / total << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int test_cases = 1;
+    // cin >> test_cases;
+    for(int tc = 1; tc <= test_cases; tc++){
+        // cout << "Case #" << tc << ": ";
+        Solve();
+    }
+    return 0;
+}

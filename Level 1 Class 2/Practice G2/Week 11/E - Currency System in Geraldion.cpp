2b21597e8cfb71cea6913b0deb03432a5adce518
Mohamed Amir <m.elamir027@gmail.com>
2023-12-17 20:02:10 +0200

    Create E - Currency System in Geraldion.cpp

---
+++ Level 1 Class 2/Practice G2/Week 11/E - Currency System in Geraldion.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    cin >> n;
+    int ans = 1;
+    for (int i = 0; i < n; ++i) {
+        int s;
+        cin >> s;
+        if (s == 1)
+            ans = -1;
+    }
+    cout << ans << endl;
+}
